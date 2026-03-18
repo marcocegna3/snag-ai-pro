fef5ecb06d7b14e460a266a4e3c8daa72416e2bd
marcocegna3 <marcocegna3@gmail.com>
2026-03-18 15:44:26 +0100

    Create shops.py

---
+++ shops.py
@@ -0,0 +1,38 @@
+# 100+ NEGOZI ITALIANI PER ARBITRAGGIO (gioielli/orologi/tuo business)
+SHOPS_IT = [
+    # GIOIELLI/OROLOGI (tuoi)
+    "https://www.chrono24.it",
+    "https://www.catawiki.com/it",
+    "https://www.pisa1940.it",
+    "https://astebolaffi.it",
+    "https://www.astagiudiziaria.com",
+    "https://www.ebay.it/sch/Orologi",
+    "https://www.santagostinoaste.it",
+    "https://www.veronikwatches.com/aste",
+    
+    # AMAZON/EBAY (arbitrage base)
+    "https://www.amazon.it/deals",
+    "https://www.ebay.it/sch/i.html?_nkw=sconti",
+    
+    # MODA/SCONTI (da topnegozi)
+    "https://www.zalando.it/privé",
+    "https://www.pittarello.com",
+    "https://www.pinalli.it",
+    "https://quellogiusto.it",
+    "https://www.quiksilver.it",
+    
+    # ELETTRONICA
+    "https://www.mediaworld.it",
+    "https://www.unieuro.it",
+    
+    # OFFERTE GENERALI
+    "https://www.groupon.it",
+    "https://www.groupalia.com/it",
+    "https://www.trovaprezzi.it",
+    
+    # +80 altri facili scrape (aggiungi tu)
+    # "https://www.awin1.com",
+    # "https://www.idealo.it",
+]
+
+print(f"Caricati {len(SHOPS_IT)} negozi italiani")
